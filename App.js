e478713cc4d4c0216af5033a39f6fb27cbcc2703
Rishi Shah <rishibshah@gmail.com>
2022-02-21 15:49:17 +0530

    added data storing

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { SafeAreaView, View, Text, TouchableOpacity, TextInput, StyleSheet, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Input from '../Test/InputScreen/Input'
+import Main from '../Test/MainScreen/Main'
 
 const Stack = createNativeStackNavigator();
 
@@ -16,81 +17,4 @@ const App = () => {
     </NavigationContainer>
   )
 };
-
-const Main = (props) => {
-  return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#fff',
-      }}>
-      {/* <View>
-        <Text style={{
-          fontSize: 30,
-          fontWeight: 'bold',
-          color: '#20315f'
-        }}>
-          User Data
-        </Text>
-      </View> */}
-      <TouchableOpacity 
-      onPress={() => props.navigation.navigate('Input')}
-      style={{ marginBottom:50 ,backgroundColor: '#87cefa', padding: 20, borderRadius: 5, flexDirection: 'row', justifyContent: 'space-between'}}>
-        <Text style={{fontWeight: 'bold', fontSize:18, color: '#fff'}}>Add User</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
-  );
-};
-
-
-const Input = (props) => {
-
-  const [FirstName, setFirstName] = React.useState("");
-  const [LastName, setLastName] = React.useState("");
-  const [Contact, setContact] = React.useState("");
-
-  return (
-    <SafeAreaView style={{flex: 1, alignItems: 'center', marginTop: 50}}>
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => setFirstName(text)}
-        value={FirstName}
-        placeholder="Enter your first name..."
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => setLastName(text)}
-        value={LastName}
-        placeholder="Enter your last name..."
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => setContact(text)}
-        value={Contact}
-        placeholder="enter your contact number..."
-        keyboardType="numeric"
-      />
-      <TouchableOpacity 
-      onPress={() => props.navigation.navigate('Main')}
-      style={{ marginTop:20 ,backgroundColor: '#87cefa', padding: 20, borderRadius: 5, flexDirection: 'row', justifyContent: 'space-between'}}>
-        <Text style={{fontWeight: 'bold', fontSize:18, color: '#fff'}}>Save </Text>
-      </TouchableOpacity>
-    </SafeAreaView>
-  )
-};
-
-const styles = StyleSheet.create({
-  input: {
-    width: 250,
-    height: 30,
-    borderColor: 'gray',
-    borderRadius: 20,
-    borderWidth: 1,
-    padding: 20,
-    marginTop: 10  
-  },
-});
-
 export default App;
---
+++ MainScreen/Main.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { SafeAreaView, Text, TouchableOpacity, FlatList, View } from 'react-native';
+
+const Main = props => {
+
+    var driver = [];
+
+    const [changeState, setChangeState] = useState(0);
+
+    if (typeof(props.route.params) != 'undefined') {
+        driver = props.route.params;
+    }
+
+    return (
+      <SafeAreaView
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#fff',
+        }}>
+        <View>
+          <Text style={{
+            fontSize: 30,
+            fontWeight: 'bold',
+            color: '#20315f'
+          }}>
+            User Data
+          </Text>
+        </View>
+
+        <FlatList 
+            data = {driver}
+            renderItem = {dataItem => (
+                <View>
+                    <Text>
+                        First Name : {dataItem.item.FirstName}
+                    </Text>
+                    <Text>
+                        Last Name : {dataItem.item.LastName}
+                    </Text>
+                    <Text>
+                        Contact Number : {dataItem.item.Contact}
+                    </Text>
+                </View>
+            )}
+        />
+
+        <TouchableOpacity
+        disabled = {changeState === 9}
+        onPress={() => {setChangeState(driver.length); props.navigation.navigate('Input', driver);}}
+        style={{ marginBottom:50 ,backgroundColor: '#87cefa', padding: 20, borderRadius: 5, flexDirection: 'row', justifyContent: 'space-between'}}>
+          <Text style={{fontWeight: 'bold', fontSize:18, color: '#fff'}}>Add User</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    );
+  };
+
+  export default Main;
