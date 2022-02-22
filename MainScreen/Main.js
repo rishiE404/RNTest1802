6abea88e51b8634ac585ab89e3a2db237e95bff1
Rishi Shah <rishibshah@gmail.com>
2022-02-22 18:18:58 +0530

    replaced navigation with push

--- MainScreen/Main.js
+++ MainScreen/Main.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
-import { SafeAreaView, Text, TouchableOpacity, FlatList, View } from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  View,
+} from 'react-native';
 
 const Main = props => {
-
   var driver = [];
 
   const [changeState, setChangeState] = useState(0);
 
-  if (typeof(props.route.params) != 'undefined') {
+  if (typeof props.route.params !== 'undefined') {
     driver = props.route.params;
   }
   // console.log('visible', props.navigation.getParam('item'))
@@ -21,11 +26,12 @@ const Main = props => {
         backgroundColor: '#fff',
       }}>
       <View>
-        <Text style={{
-          fontSize: 30,
-          fontWeight: 'bold',
-          color: '#20315f'
-        }}>
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: 'bold',
+            color: '#20315f',
+          }}>
           User Data
         </Text>
       </View>
@@ -34,15 +40,9 @@ const Main = props => {
         data={driver}
         renderItem={dataItem => (
           <View>
-            <Text>
-              First Name : {dataItem.item.FirstName}
-            </Text>
-            <Text>
-              Last Name : {dataItem.item.LastName}
-            </Text>
-            <Text>
-              Contact Number : {dataItem.item.Contact}
-            </Text>
+            <Text>First Name : {dataItem.item.FirstName}</Text>
+            <Text>Last Name : {dataItem.item.LastName}</Text>
+            <Text>Contact Number : {dataItem.item.Contact}</Text>
           </View>
         )}
       />
@@ -50,9 +50,21 @@ const Main = props => {
       <View>
         <TouchableOpacity
           disabled={changeState === 9}
-          onPress={() => { setChangeState(driver.length); props.navigation.navigate('Input', driver); }}
-          style={{ marginBottom: 50, backgroundColor: '#87cefa', padding: 20, borderRadius: 5, flexDirection: 'row', justifyContent: 'space-between' }}>
-          <Text style={{ fontWeight: 'bold', fontSize: 18, color: '#fff' }}>Add User</Text>
+          onPress={() => {
+            setChangeState(driver.length);
+            props.navigation.navigate('Input', driver);
+          }}
+          style={{
+            marginBottom: 50,
+            backgroundColor: '#87cefa',
+            padding: 20,
+            borderRadius: 5,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}>
+          <Text style={{ fontWeight: 'bold', fontSize: 18, color: '#fff' }}>
+            Add User
+          </Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
